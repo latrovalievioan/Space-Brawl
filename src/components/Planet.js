46a2575b23610825bf60df18d401da9d0e0e496e
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-03-24 11:22:54 +0200

    docs: jsDoc for Planet.js

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -4,9 +4,16 @@ import Rover from "./Rover";
 import gsap from "gsap/all";
 import { random } from "../core/utils";
 import Shield from "./Shield";
-import Rocket from "./Rocket";
 
+/**
+ * Represents a planet.
+ * @class
+ */
 export default class Planet extends Container {
+  /**
+   * @param {{string, number, number, number, Object, Object, Object}} Object
+   * @param {string} name
+   */
   constructor({ image, x = 0, y = 0, scale, rover, shield, rocket }, name) {
     super();
     this._image = image;
@@ -20,6 +27,11 @@ export default class Planet extends Container {
     this.name = name;
   }
 
+  /**
+   * Draws a planet on the canvas.
+   * @method
+   * @private
+   */
   _createPlanet() {
     this._body = new Sprite.from(this._image);
     this._body.anchor.set(0.5);
@@ -29,19 +41,34 @@ export default class Planet extends Container {
     this.addChild(this._body);
     if (this._roverConfig) this._createRover();
     if (this._shieldConfig) this._createShield();
-
     this._floatAnimation();
   }
 
+  /**
+   * Draws a rover.
+   * @method
+   * @private
+   */
   _createRover() {
     this._rover = new Rover(this._roverConfig);
     this._body.addChild(this._rover);
   }
+
+  /**
+   * Draws a shield.
+   * @method
+   * @private
+   */
   _createShield() {
     this.shield = new Shield(this._shieldConfig);
     this._body.addChild(this.shield);
   }
 
+  /**
+   * Animates the planet so that it "floats".
+   * @method
+   * @private
+   */
   _floatAnimation() {
     this._tl = gsap.timeline();
     this._tl.to(this, {
